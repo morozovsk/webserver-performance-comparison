f39800b13418ea3b9f2f3570e6eb2de400141ebf
Vladimir Goncharov <morozovsk@gmail.com>
2018-01-10 00:07:14 +0200

    Create fasthttp.go

---
+++ fasthttp.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	addr     = flag.String("addr", ":1080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, world!")
+}
