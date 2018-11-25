6ad99ea9f8b6ae1da56b339c54c63f9ab857c415
Vladimir Goncharov <morozovsk@gmail.com>
2018-11-25 02:29:40 +0200

    Update workerman.php

--- workerman.php
+++ workerman.php
@@ -1,15 +1,13 @@
 <?php
 
-//second test:
 //pecl install event
 
-require_once DIR . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
-
-//second test:
-//Worker::$eventLoopClass = '\Workerman\Events\Ev';
 
 $http_worker = new Worker("http://0.0.0.0:1080");
 $http_worker->count = 1;
 $http_worker->onMessage = function($conn, $data) {$conn->send("hello world");};
 Worker::runAll();
+
+//command: php workerman.php start
