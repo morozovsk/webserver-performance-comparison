197375f45101569dc55ed4f7df64925eeaf35d23
Vladimir Goncharov <morozovsk@gmail.com>
2018-11-25 02:27:47 +0200

    Update swoole.php

--- swoole.php
+++ swoole.php
@@ -4,9 +4,9 @@
 
 //first test
 $server = new Swoole\Http\Server('0.0.0.0', 1080);
-//second test:
-//$serv = new Swoole\Http\Server('0.0.0.0', 1080, SWOOLE_BASE);
 
 $server->set(['worker_num' => 1,]);
 $server->on('Request', function($req, $res) {$res->end('hello world');});
 $server->start();
+
+//command: php swoole.php
