dbbfee9b68ef4ac9965ed2c21e5739d0ea843a6c
Vladimir Goncharov <morozovsk@gmail.com>
2018-11-25 02:16:44 +0200

    Create roadrunner.php

---
+++ roadrunner.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 'stderr');
+include "vendor/autoload.php";
+
+$relay = new Spiral\Goridge\StreamRelay(STDIN, STDOUT);
+$psr7 = new Spiral\RoadRunner\PSR7Client(new Spiral\RoadRunner\Worker($relay));
+
+while ($req = $psr7->acceptRequest()) {
+    try {
+        $resp = new \Zend\Diactoros\Response();
+        $resp->getBody()->write("hello world");
+
+        $psr7->respond($resp);
+    } catch (\Throwable $e) {
+        $psr7->getWorker()->error((string)$e);
+    }
+}
